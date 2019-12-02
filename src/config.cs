8d7a74e6eb22080e4f062d34c6ba2b5183eba559
Bart Robertson <bartr@microsoft.com>
2019-12-02 10:44:50 -0600

    convert config to use properties instead of fields (#61)

--- src/config.cs
+++ src/config.cs
@@ -4,14 +4,14 @@ namespace Helium
 {
     public class Config
     {
-        public bool Verbose = false;
-        public bool Random = false;
-        public int SleepMs = -1;
-        public int Threads = -1;
-        public int Duration = 0;
-        public bool RunLoop = false;
-        public bool RunWeb = false;
-        public string Host = string.Empty;
-        public readonly List<string> FileList = new List<string>();
+        public string Host { get; set; } = string.Empty;
+        public bool RunLoop { get; set; } = false;
+        public bool RunWeb { get; set; } = false;
+        public int Threads { get; set; } = -1;
+        public int SleepMs { get; set; } = -1;
+        public int Duration { get; set; } = 0;
+        public bool Random { get; set; } = false;
+        public bool Verbose { get; set; } = false;
+        public List<string> FileList { get; } = new List<string>();
     }
 }
