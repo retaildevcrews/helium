eee93572e84768c2ef42b7ea12c457d792b0e131
bartr <bartr@outlook.com>
2019-12-22 15:26:48 -0600

    resolved fxcop warnings

--- src/Middleware/requestLogger.cs
+++ src/Middleware/requestLogger.cs
@@ -8,10 +8,10 @@ namespace Helium
 {
     public class LoggerOptions
     {
-        public bool Log2xx = true;
-        public bool Log3xx = true;
-        public bool Log4xx = true;
-        public bool Log5xx = true;
+        public bool Log2xx { get; set; } = true;
+        public bool Log3xx { get; set; } = true;
+        public bool Log4xx { get; set; } = true;
+        public bool Log5xx { get; set; } = true;
     }
 
     public static class LoggerMiddlewareExtensions
@@ -41,7 +41,7 @@ namespace Helium
         {
             // save for later
             _next = next;
-            _options = options.Value;
+            _options = options?.Value;
         }
 
         public async Task Invoke(HttpContext context)
@@ -52,7 +52,7 @@ namespace Helium
             // Invoke next handler
             if (_next != null)
             {
-                await _next.Invoke(context);
+                await _next.Invoke(context).ConfigureAwait(false);
             }
 
             if (context.Response.StatusCode < 300)
