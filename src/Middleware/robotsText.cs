bc74f6073ec27c744a8f0b6eff48d46678486379
bartr <bartr@outlook.com>
2019-12-22 15:28:05 -0600

    resolved fxcop warnings

--- src/Middleware/robotsText.cs
+++ src/Middleware/robotsText.cs
@@ -21,20 +21,20 @@ namespace Helium
             builder.Use(async (context, next) =>
             {
                 // convert to lower and remove the leading /
-                string path = context.Request.Path.Value.ToLower().Substring(1);
+                string path = context.Request.Path.Value.Substring(1);
 
                 // matches /robots*.txt (/robots.txt /robots123.txt etc)
                 // does not match /robots/robots.txt
-                if (!path.Contains("/") && path.StartsWith("robots") && path.EndsWith(".txt"))
+                if (!path.Contains("/", System.StringComparison.OrdinalIgnoreCase) && path.StartsWith("robots", System.StringComparison.OrdinalIgnoreCase) && path.EndsWith(".txt", System.StringComparison.OrdinalIgnoreCase))
                 {
                     // return the content
                     context.Response.ContentType = "text/plain";
-                    await context.Response.Body.WriteAsync(_responseBytes, 0, _responseBytes.Length);
+                    await context.Response.Body.WriteAsync(_responseBytes, 0, _responseBytes.Length).ConfigureAwait(false);
                 }
                 else
                 {
                     // not a match, so call next middleware handler
-                    await next();
+                    await next().ConfigureAwait(false);
                 }
             });
 
