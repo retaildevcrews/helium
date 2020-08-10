e204c6df54d3ef52ad3786ead2b9c09fcc721e27
Swapnil Jha <swapniljha001@gmail.com>
2020-08-10 22:59:58 +0530

    Update aad-podid.sh (#508)
    * Update aad-podid.sh
    
    ## Type of PR
    
    - [x] Documentation changes
    - [ ] Code changes
    
    ### Purpose of PR
    
    Changed reference(s) of old service named MSI to MI.
     
    ### Issues Closed or Referenced
    
    - References #<501> (this references the issue but does not close with PR)
    
    * Update aad-podid.sh

--- docs/aks/aad-podid.sh
+++ docs/aks/aad-podid.sh
@@ -4,7 +4,7 @@ do
  case "${option}" in
  a) AKS_NAME=${OPTARG};;
  r) AKS_RG=${OPTARG};;
- m) MSI_NAME=${OPTARG};;
+ m) MI_NAME=${OPTARG};;
  *) echo "Please refer to usage guide on GitHub" >&2
     exit 1 ;;
  esac
@@ -46,42 +46,42 @@ if ! kubectl get deploy mic > /dev/null 2>&1; then
     fi
 fi
 
-echo "creating azure user assigned managed identity $MSI_NAME in the $AKS_NODE_RG Resource Group"
-if echo $AKS_NODE_RG > /dev/null 2>&1 && echo $MSI_NAME > /dev/null 2>&1; then
-    if ! export MSI_PrincID=$(az identity create -g ${AKS_NODE_RG} -n ${MSI_NAME} --query principalId -o tsv); then
+echo "creating azure user assigned managed identity $MI_NAME in the $AKS_NODE_RG Resource Group"
+if echo $AKS_NODE_RG > /dev/null 2>&1 && echo $MI_NAME > /dev/null 2>&1; then
+    if ! export MI_PrincID=$(az identity create -g ${AKS_NODE_RG} -n ${MI_NAME} --query principalId -o tsv); then
         echo "ERROR: failed to create managed identity"
         exit 1
     fi
-    echo "Managed Identity Principal ID = $MSI_PrincID"
+    echo "Managed Identity Principal ID = $MI_PrincID"
 fi
 
-echo "getting azure user assigned managed identity $MSI_NAME resource ID"
-if echo $AKS_NODE_RG > /dev/null 2>&1 && echo $MSI_NAME > /dev/null 2>&1; then
-    if ! export MSI_ResID=$(az identity show -g ${AKS_NODE_RG} -n ${MSI_NAME} --query id -o tsv); then
+echo "getting azure user assigned managed identity $MI_NAME resource ID"
+if echo $AKS_NODE_RG > /dev/null 2>&1 && echo $MI_NAME > /dev/null 2>&1; then
+    if ! export MI_ResID=$(az identity show -g ${AKS_NODE_RG} -n ${MI_NAME} --query id -o tsv); then
         echo "ERROR: failed to get managed identity resource ID"
         exit 1
     fi
-    echo "Managed Identity Resource ID = $MSI_ResID"
+    echo "Managed Identity Resource ID = $MI_ResID"
 fi
 
-echo "getting azure user assigned managed identity $MSI_NAME client ID"
-if echo $AKS_NODE_RG > /dev/null 2>&1 && echo $MSI_NAME > /dev/null 2>&1; then
-    if ! export MSI_ClientID=$(az identity show -g ${AKS_NODE_RG} -n ${MSI_NAME} --query clientId -o tsv); then
+echo "getting azure user assigned managed identity $MI_NAME client ID"
+if echo $AKS_NODE_RG > /dev/null 2>&1 && echo $MI_NAME > /dev/null 2>&1; then
+    if ! export MI_ClientID=$(az identity show -g ${AKS_NODE_RG} -n ${MI_NAME} --query clientId -o tsv); then
         echo "ERROR: failed to get managed identity resource ID"
         exit 1
     fi
-    echo "Managed Identity Client ID = $MSI_ClientID"
+    echo "Managed Identity Client ID = $MI_ClientID"
 fi
 
-while ! az role assignment create --role Reader --assignee $MSI_PrincID --scope $AKS_NODE_RG_RESID
+while ! az role assignment create --role Reader --assignee $MI_PrincID --scope $AKS_NODE_RG_RESID
 do
   echo "Sleeping for 10 seconds waiting for AAD Propogation of Identity"
   sleep 10s
 done
 
-echo "assigning the managed identity Principal ID $MSI_PrincID reader role to  the $AKS_NODE_RG Resource Group"
-if echo $MSI_PrincID > /dev/null 2>&1 && echo $AKS_NODE_RG > /dev/null 2>&1; then
-    if ! az role assignment create --role Reader --assignee $MSI_PrincID --scope $AKS_NODE_RG_RESID; then
+echo "assigning the managed identity Principal ID $MI_PrincID reader role to  the $AKS_NODE_RG Resource Group"
+if echo $MI_PrincID > /dev/null 2>&1 && echo $AKS_NODE_RG > /dev/null 2>&1; then
+    if ! az role assignment create --role Reader --assignee $MI_PrincID --scope $AKS_NODE_RG_RESID; then
         echo "ERROR: failed to assign the reader role to the managed identity"
         exit 1
     fi
@@ -105,16 +105,16 @@ if echo $AKS_NAME > /dev/null 2>&1 && echo $AKS_RG >/dev/null 2>&1; then
 fi
 
 echo "assigning the AKS Service Principal Managed Identity Operator rights"
-if echo $AKS_SP_ObjID > /dev/null 2>&1 && echo $MSI_ResID > /dev/null 2>&1; then
-    if ! az role assignment create --role "Managed Identity Operator" --assignee $AKS_SP_ObjID --scope $MSI_ResID; then
+if echo $AKS_SP_ObjID > /dev/null 2>&1 && echo $MI_ResID > /dev/null 2>&1; then
+    if ! az role assignment create --role "Managed Identity Operator" --assignee $AKS_SP_ObjID --scope $MI_ResID; then
         echo "ERROR: failed to assign the AKS Service Principal Managed Identity Operator rights"
         exit 1
     fi
 fi
 
 # echo "assigning the Managed Identity access rights to the Azure Key Vault"
-# if echo $KEY_VAULT_NAME > /dev/null 2>&1 && echo $MSI_PrincID > /dev/null 2>&1; then
-#     if ! az keyvault set-policy -n $KEY_VAULT_NAME --object-id $MSI_PrincID --secret-permissions get list --key-permissions get list --certificate-permissions get list; then
+# if echo $KEY_VAULT_NAME > /dev/null 2>&1 && echo $MI_PrincID > /dev/null 2>&1; then
+#     if ! az keyvault set-policy -n $KEY_VAULT_NAME --object-id $MI_PrincID --secret-permissions get list --key-permissions get list --certificate-permissions get list; then
 #         echo "ERROR: failed to assign the Managed Identity access rights to the Azure Key Vault"
 #         exit 1
 #     fi
@@ -123,39 +123,39 @@ fi
 
 
 echo "The aadpodidentity resource will be deployed to cluster $AKS_NAME. It will be saved to your current directory as aadpodidentity.yaml"
-cat << EOF > cluster/manifests/aadpodidentity/${MSI_NAME}aadpodidentity.yaml
+cat << EOF > cluster/manifests/aadpodidentity/${MI_NAME}aadpodidentity.yaml
 apiVersion: "aadpodidentity.k8s.io/v1"
 kind: AzureIdentity
 metadata:
- name: $MSI_NAME
+ name: $MI_NAME
 spec:
  type: 0
- ResourceID: $MSI_ResID
- ClientID: $MSI_ClientID
+ ResourceID: $MI_ResID
+ ClientID: $MI_ClientID
 EOF
 
 echo "deploying aadpodidentity resource into the default namespace"
 if kubectl get deploy mic > /dev/null 2>&1; then
-    if ! kubectl apply -f cluster/manifests/aadpodidentity/${MSI_NAME}aadpodidentity.yaml; then
+    if ! kubectl apply -f cluster/manifests/aadpodidentity/${MI_NAME}aadpodidentity.yaml; then
         echo "ERROR: failed to create kubernetes aadpodidenity resource"
         exit 1
     fi
 fi
 
 echo "The aadpodidentitybiding resource will be deployed to cluster $AKS_NAME. It will be saved to your current directory as aadpodidentitybinding.yaml"
-cat << EOF > cluster/manifests/aadpodidentity/${MSI_NAME}aadpodidentitybinding.yaml
+cat << EOF > cluster/manifests/aadpodidentity/${MI_NAME}aadpodidentitybinding.yaml
 apiVersion: "aadpodidentity.k8s.io/v1"
 kind: AzureIdentityBinding
 metadata:
- name: ${MSI_NAME}-binding
+ name: ${MI_NAME}-binding
 spec:
- AzureIdentity: ${MSI_NAME}
- Selector: ${MSI_NAME}
+ AzureIdentity: ${MI_NAME}
+ Selector: ${MI_NAME}
 EOF
 
 echo "creating deploying aadpodidentitybinding resource into the default namespace"
 if kubectl get deploy mic > /dev/null 2>&1; then
-    if ! kubectl apply -f cluster/manifests/aadpodidentity/${MSI_NAME}aadpodidentitybinding.yaml; then
+    if ! kubectl apply -f cluster/manifests/aadpodidentity/${MI_NAME}aadpodidentitybinding.yaml; then
         echo "ERROR: failed to create kubernetes aadpodidenitybinding resource"
         exit 1
     fi
@@ -163,17 +163,17 @@ fi
 
 echo " "
 echo "******************************************************************************************************************"
-echo "AAD Pod Identity has been deployed to you cluster $AKS_NAME and is using $MSI_NAME for its managed Identity"
-echo "Now you can configure ${MSI_NAME} to have access rights to any azure resource based on Azure IAM roles"
+echo "AAD Pod Identity has been deployed to you cluster $AKS_NAME and is using $MI_NAME for its managed Identity"
+echo "Now you can configure ${MI_NAME} to have access rights to any azure resource based on Azure IAM roles"
 echo "******************************************************************************************************************"
 echo " "
 echo "******************************************************************************************************************"
-echo "To assign the MSI to your keyvault run the following:"
-echo "az keyvault set-policy -n ${He_Name} --object-id ${MSI_PrincID} --secret-permissions get list \\"
+echo "To assign the MI to your keyvault run the following:"
+echo "az keyvault set-policy -n ${He_Name} --object-id ${MI_PrincID} --secret-permissions get list \\"
 echo "--key-permissions get list --certificate-permissions get list"
 echo "******************************************************************************************************************"
 echo " "
 echo "******************************************************************************************************************"
-echo "Add the label below to your deployment to assign an aad MSi to those pods:"
-echo " aadpodidentity: ${MSI_NAME}"
+echo "Add the label below to your deployment to assign an aad MI to those pods:"
+echo " aadpodidentity: ${MI_NAME}"
 echo "******************************************************************************************************************"
